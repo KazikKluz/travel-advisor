1b0c0580bb2a1354c31d5df0de850bd336d565e4
Kazik Kluz <kazik.kluz@gmail.com>
2022-02-18 18:14:13 +0000

    create simple app bar with search field

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
+
+import Header from './components/header/header.component';
+import List from './components/list/list.component';
+import Map from './components/map/map.component';
+import PlaceDetails from './components/placeDetails/placeDetails';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <List />
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
