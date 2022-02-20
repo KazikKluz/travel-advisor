83a64c95ead69c3a5415fcae771f400b98880194
Kazik Kluz <kazik.kluz@gmail.com>
2022-02-20 08:05:55 +0000

    connected to google maps, reading current geolocation and passing it to rapid api for getting restaurants data, displaying cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { CssBaseline, Grid } from '@material-ui/core';
+
+import { getPlacesData } from './api/index';
 
 import Header from './components/header/header.component';
 import List from './components/list/list.component';
@@ -7,16 +9,39 @@ import Map from './components/map/map.component';
 import PlaceDetails from './components/placeDetails/placeDetails';
 
 const App = () => {
+  const [places, setPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState({});
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        console.log(latitude, longitude);
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    getPlacesData(bounds.sw, bounds.ne).then((data) => {
+      setPlaces(data);
+    });
+  }, [coordinates, bounds]);
+
   return (
     <>
       <CssBaseline />
       <Header />
       <Grid container spacing={3} style={{ width: '100%' }}>
         <Grid item xs={12} md={4}>
-          <List />
+          <List places={places} />
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map />
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+          />
         </Grid>
       </Grid>
     </>
